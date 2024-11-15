9078394d0753a8556f721671d139febae345ea5d
Aloysius Chandra <aloysiuschandra3@gmail.com>
2024-11-15 14:41:38 +0700

    fixing the manifest and service worker

--- service-worker.js
+++ service-worker.js
@@ -4,19 +4,22 @@ const urlsToCache = [
     "/index.html",
     "/gps.html",
     "/manifest.json",
-    "/style.css",
-    // "/script.js"
+    "/screenshot1.png",
+    "/screenshot2.png"
 ];
 
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(urlsToCache);
+            return cache.addAll(urlsToCache).then(() => {
+                console.log("All resources cached successfully!");
+            }).catch((err) => {
+                console.error("Resource failed to cache:", err);
+            });
         })
     );
 });
-
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
